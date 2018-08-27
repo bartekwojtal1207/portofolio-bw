e6c607010bf24bb2656bf4ce8cc52bfc06547059
Bartek <bartekwojtal1207@gmail.com>
2018-08-27 15:01:53 +0200

    add form message ( object inputs  in state)

--- src/component/AboutMeSection/AboutMeSection.js
+++ src/component/AboutMeSection/AboutMeSection.js
@@ -1,14 +1,53 @@
 import React, {Component} from 'react';
 import Auxs from '../../hoc/Auxs';
 
+import './AboutMeSection.css';
+
 
 class AboutMeSection extends Component{
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+            formContact : {
+               inputs : {
+                   name : {
+                       name: 'name',
+                       type: 'text',
+                       required: true
+                   },
+                   email : {
+                       name: 'email',
+                       type: 'email',
+                       required: true
+                   },
+                   message : {
+                       name: 'message',
+                       type: 'textarea',
+                       required: true
+                   }
+
+               }
+            }
+        }
+    }
 
     render() {
         return(
-            <div style={{height: '100vh'}}>
-                dasdadasdadasd1111111111111
+            <div style={{height: '100vh'}} className={'about-me-wrapper'}>
+                <div className={'form-wrapper'}>
+                    <h3 className={'form-title'}>Napisz do mnie !</h3>
+                    <form action="">
+                        <div className="form-group">
+                            <input type={'text'} className={'form-control'}/>
+                        </div>
+                        <br/>
+                        <button role={'submit'} className={'btn btn-info form-submit'}>wyślij</button>
+                    </form>
+                </div>
+
             </div>
         )
     }
